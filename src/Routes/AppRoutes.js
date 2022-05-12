5cf1300754f0b046180e24f719236a23e562344c
Phil Olalere <olalerephilip@gmail.com>
2022-05-12 22:47:29 +0100

    added some route and server config

---
+++ src/Routes/AppRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer')
+const CarController = require('../Controllers/CarController');
+const path = require('path')
+// const auth = require("../middleware/Auth");
+
+let imageName = "";
+var storage = multer.diskStorage({
+    
+    destination: function (req, file, cb) {
+      cb(null, process.cwd()+'/public/storage/')
+    },
+    filename: function (req, file, cb) {
+        imageName = Date.now() + path.extname(file.originalname);
+        console.log(imageName);
+      cb(null, file.fieldname+"_"+Date.now()+file.originalname);
+    }
+
+    
+})
+var upload = multer({ storage: storage })
+
+
+router.use(express.static(__dirname + '/public'));
+router.use('/uploads', express.static('uploads'));
+
+router.post('/createCar', upload.single('image'), CarController.CreateCar);
+router.post('/getAllProducts', CarController.FetchAllCars);
+router.post('/filterProducts', CarController.JoinProductsWithUser);
+module.exports = router;
